7fd26738c49b002f49cfbf66567f6e406c27a389
u25555202-Francois <u25555202@tuks.co.za>
2026-08-22 16:54:34 +0200

    Dynamic  makefile. Do not modify

---
+++ src/EventGroups/testing.cpp
@@ -0,0 +1,5 @@
+#include "testing.h"
+#include <iostream>
+void test(){
+    std::cout<<"Test ran\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+//Include format: #include "EventGroups/testing.h"
+
+int main()
+{
+    
+    std::cout<<"Main ran\n";
+
+    return 0;
+}
