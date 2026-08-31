8e57c70f50802de717c5e29cbc6c719e3b66135a
wbotha1019-crypto <wbotha1019@gmail.com>
2026-08-31 15:45:07 +0200

    Refactor getPlayersOnHole method logic
    Refactor getPlayersOnHole to separate players staying on the hole from those being removed.

--- src/EventUnits/PlayerTent.cpp
+++ src/EventUnits/PlayerTent.cpp
@@ -5,14 +5,18 @@
 std::vector<Player*> PlayerTent::getPlayersOnHole(int hole) {
 	std::vector<Player*> retPlayers;
 
+	std::vector<Player*> staying;
+
 	for (std::size_t i = 0;i < players.size();i++) {
 		if (players[i]->getState().holesPlayed == hole) {
 			retPlayers.push_back(players[i]);
 		}
+		else
+		{
+			staying.push_back(players[i]);
+		}
 	}
-	for (auto player : players) {
-		removePlayer(player);
-	}
+	players = staying;
 
 
 	return retPlayers;
